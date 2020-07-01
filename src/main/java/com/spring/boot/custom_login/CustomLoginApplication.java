d0fedabc615b83db50670abbf7cfdc714accb614
waqasahmedNU <k122193@nu.edu.pk>
2020-07-01 16:22:53 +0200

    Initial project files commit

---
+++ src/main/java/com/spring/boot/custom_login/CustomLoginApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.custom_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomLoginApplication.class, args);
+    }
+
+}
